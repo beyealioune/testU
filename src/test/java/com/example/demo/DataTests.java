5113c9c3d454849135475c662a58f2e168a53e4a
BEYE ALIOUNE <alioune.beye@gmail.com>
2023-05-31 10:46:00 +0200

    veteran

--- src/test/java/com/example/demo/DataTests.java
+++ src/test/java/com/example/demo/DataTests.java
@@ -1,24 +1,21 @@
-package com.example.demo;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.Optional;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import com.example.demo.model.Fire;
 import com.example.demo.model.Fireman;
 import com.example.demo.repository.FireRepository;
 import com.example.demo.repository.FiremanRepository;
 
-@DataJpaTest
- public class DataTests {
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Optional;
 
+@DataJpaTest
+public class DataTests {
 
     @Autowired
     FireRepository fireRepository;
@@ -26,38 +23,65 @@ import com.example.demo.repository.FiremanRepository;
     @Autowired
     FiremanRepository firemanRepository;
 
-
-
+    @BeforeEach
+    public void setup() {
+        fireRepository.deleteAll();
+        firemanRepository.deleteAll();
+    }
 
     @Test
-    public void testCreateFireAndFireman() {
-        int severity = 8;
-        Instant date = Instant.now();
-    
-        // Création des feux
-        Fire fire1 = new Fire(severity, date);
-        Fire fire2 = new Fire(severity, date);
-    
-        // Sauvegarde des feux dans la base de données
-        fireRepository.save(fire1);
-        fireRepository.save(fire2);
-    
-        // Création du pompier
-        Fireman fireman = new Fireman();
-        fireman.setName("John Doe");
-    
-        // Association du pompier aux feux
-        fireman.setExtinguishedFires(Arrays.asList(fire1, fire2));
-    
-        // Sauvegarde du pompier dans la base de données
-        firemanRepository.save(fireman);
-    
-        // Vérification des associations
-        Optional<Fireman> firemanFromDB = firemanRepository.findById(fireman.getId());
-        assertTrue(firemanFromDB.isPresent());
-        assertEquals(fireman.getId(), firemanFromDB.get().getId());
-        assertEquals(fire1.getId(), firemanFromDB.get().getExtinguishedFires().get(0).getId());
-        assertEquals(fire2.getId(), firemanFromDB.get().getExtinguishedFires().get(1).getId());
+    public void testGetVeteranWithMultipleFiremen() {
+        //  creer des nouveau pompier
+        Fireman fireman1 = createFiremanWithFires("John Doe", 5);
+        Fireman fireman2 = createFiremanWithFires("Jane Smith", 3);
+        Fireman fireman3 = createFiremanWithFires("Mike Johnson", 8);
+
+        // Save en base de donné 
+        firemanRepository.saveAll(Arrays.asList(fireman1, fireman2, fireman3));
+
+        // retrouvé le vétéran 
+        Optional<Fireman> veteran = firemanRepository.getVeteran();
+
+        // check resulta 
+        Assertions.assertTrue(veteran.isPresent());
+        Assertions.assertEquals("Mike Johnson", veteran.get().getName());
     }
-    
+
+    @Test
+    public void testGetVeteranWithSingleFireman() {
+        // Create a fireman with fires
+        Fireman fireman = createFiremanWithFires("John Doe", 10);
+
+        // Save fireman in bdd
+        firemanRepository.save(fireman);
+
+        // get veteran 
+        Optional<Fireman> veteran = firemanRepository.getVeteran();
+
+        // check resultat
+        Assertions.assertTrue(veteran.isPresent());
+        Assertions.assertEquals("John Doe", veteran.get().getName());
+    }
+
+    @Test
+    public void testGetVeteranWithNoFireman() {
+        // Retrouver le veteran 
+        Optional<Fireman> veteran = firemanRepository.getVeteran();
+
+        // Verify the result
+        Assertions.assertTrue(veteran.isEmpty());
+    }
+
+    private Fireman createFiremanWithFires(String name, int numFires) {
+        Fireman fireman = new Fireman();
+        fireman.setName(name);
+
+        for (int i = 0; i < numFires; i++) {
+            Fire fire = new Fire(8, Instant.now());
+            fireman.getExtinguishedFires().add(fire);
+            fireRepository.save(fire);
+        }
+
+        return fireman;
+    }
 }
